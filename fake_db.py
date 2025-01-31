d4176e28d7ea6768a344d4fb7172d4825d3b3701
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-01-31 18:12:53 +0200

    Add fake_db.py file with test data

---
+++ fake_db.py
@@ -0,0 +1,22 @@
+fake_db = [
+    {
+        "id": 1,
+        "title": "Test Title 1",
+        "description": "Test some description 1",
+    },
+    {
+        "id": 2,
+        "title": "Test Title 2",
+        "description": "Test some description 2",
+    },
+    {
+        "id": 3,
+        "title": "Test Title 3",
+        "description": "Test some description 3",
+    },
+    {
+        "id": 4,
+        "title": "Test Title 4",
+        "description": "Test some description 4",
+    }
+]
