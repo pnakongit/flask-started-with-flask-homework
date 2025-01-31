806eb19c9810d135d66f110d135203ad31878f1f
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-01-31 20:09:39 +0200

    Add status field to data in fake_db.py

--- fake_db.py
+++ fake_db.py
@@ -3,20 +3,24 @@ fake_db = [
         "id": 1,
         "title": "Test Title 1",
         "description": "Test some description 1",
+        "status": False
     },
     {
         "id": 2,
         "title": "Test Title 2",
         "description": "Test some description 2",
+        "status": False
     },
     {
         "id": 3,
         "title": "Test Title 3",
         "description": "Test some description 3",
+        "status": False
     },
     {
         "id": 4,
         "title": "Test Title 4",
         "description": "Test some description 4",
+        "status": False
     }
 ]
