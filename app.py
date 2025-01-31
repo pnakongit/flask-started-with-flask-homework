fe10ff7e7781ac2471e48959a11a259470d3c60f
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2025-01-31 18:23:23 +0200

    Add initial route definitions for task management

--- app.py
+++ app.py
@@ -8,5 +8,25 @@ def hello_world() -> str:
     return "<h1>Hello, World!</h1>"
 
 
+@app.route("/tasks", methods=["GET"])
+def todo_list() -> str:
+    return ""
+
+
+@app.route("/tasks/create", methods=["POST"])
+def todo_create() -> str:
+    return ""
+
+
+@app.route("/tasks/<int:pk>/change_status", methods=["POST"])
+def todo_change_status(pk: int) -> str:
+    return ""
+
+
+@app.route("/tasks/<int:pk>/delete", methods=["POST"])
+def todo_delete(pk: int) -> str:
+    return ""
+
+
 if __name__ == "__main__":
     app.run()
